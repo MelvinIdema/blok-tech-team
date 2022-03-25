ce419775388f26684111a64076db6aa308c4d591
Melvin Idema <melvin_i13@hotmail.com>
2022-03-25 14:54:20 +0100

    #21 Add code structure

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,8 +1,23 @@
 async function login(req, res) {
-  return res.send('NOT IMPLEMENTED: LOGIN');
-  // login
+  if (req.method === 'GET') return res.send('GET LOGIN');
+
+  return res.send('NOT IMPLEMENTED: POST LOGIN');
+}
+
+async function register(req, res) {
+  if (req.method === 'GET') return res.send('GET REGISTER');
+
+  return res.send('POST REGISTER');
+}
+
+async function account(req, res) {
+  if (req.method === 'GET') return res.send('GET ACCOUNT');
+
+  return res.send('POST ACCOUNT');
 }
 
 module.exports = {
   login,
+  register,
+  account,
 };
