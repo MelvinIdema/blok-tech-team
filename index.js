ee5f94b486881ee2d937a4035c422c4769290c9b
sharon <svanopzeeland@kpnmail.nl>
2022-03-26 13:57:24 +0100

    #29 add express and ejs, change css to src folder

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const UserRouter = require('./routes/User.js')
-const AppRouter = require('./routes/App.js')
+const UserRouter = require('./routes/User.js');
+const AppRouter = require('./routes/App.js');
 const app = express();
 
 app.use('/user', UserRouter);
 app.use('/', AppRouter);
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.listen(3000, () => console.log('Listening on port: 3000'));
