27405cbf7ea0bcfe84c7a15b3bcb3084825d91e9
Melvin Idema <melvin_i13@hotmail.com>
2022-03-28 17:05:32 +0200

    #40 fix authenticated middleware on homepage

--- routes/App.js
+++ routes/App.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const AppController = require('./../app/controllers/AppController.js');
-const authenticated = require('./../app/middlewares/authenticated.js');
 const AppRouter = express.Router();
 
-AppRouter.use(authenticated);
 AppRouter.get('/', AppController.home);
 
 module.exports = AppRouter;
