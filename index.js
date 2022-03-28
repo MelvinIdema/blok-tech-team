83e70f33153a73a2a168ea1f7d7d03c5d3116aa3
danajamilla <danajamilla@hotmail.com>
2022-03-28 14:58:06 +0200

    #26 opzet HTML en CSS

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const UserRouter = require('./routes/User.js');
 const AppRouter = require('./routes/App.js');
+const AppointmentRouter = require('./routes/Appointment.js');
 const app = express();
 
 app.use(express.static('public'));
 app.use('/user', UserRouter);
 app.use('/', AppRouter);
+app.use('/appointment', AppointmentRouter);
 
 app.set('view engine', 'ejs');
 
