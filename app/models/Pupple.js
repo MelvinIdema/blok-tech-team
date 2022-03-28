c20e76e98d5cf8a2f51761bf832144f0e250f598
Bram-ter <bram.tersteeg2@hva.nl>
2022-03-28 15:00:22 +0200

    #30 add more datatypes to schema

--- app/models/Pupple.js
+++ app/models/Pupple.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 mongoose.connect(process.env.DB_URI);
+
 const puppleSchema = new Schema({
   name: String,
+  location: String,
   age: Number,
+  personality: String,
+  walkDuration: Number,
 });
 
-module.exports('Pupple', puppleSchema);
+module.exports = mongoose.model('Pupple', puppleSchema);
