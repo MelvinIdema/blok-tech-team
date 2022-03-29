4d9895c7fcf6cbcab1cf23e0ba7af4bd4e934ade
Melvin Idema <melvin_i13@hotmail.com>
2022-03-29 14:48:35 +0200

    #55 Add Pupple API route & controller

--- app/controllers/PuppleController.js
+++ app/controllers/PuppleController.js
@@ -1,3 +1,5 @@
+const Pupple = require('../models/Pupple.js');
+
 function cards(req, res) {
   return res.render('pupplecards');
 }
@@ -6,7 +8,12 @@ function info(req, res) {
   return res.render('info');
 }
 
+async function api(req, res) {
+  return res.json(await Pupple.find({}));
+}
+
 module.exports = {
   cards,
   info,
+  api,
 };
