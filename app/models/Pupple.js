a8af482a1a942bebd1aeb868dd41be274561d050
Bram-ter <bram.tersteeg2@hva.nl>
2022-03-29 14:58:17 +0200

    #30 update pupple schema

--- app/models/Pupple.js
+++ app/models/Pupple.js
@@ -4,11 +4,13 @@ const { Schema } = mongoose;
 mongoose.connect(process.env.DB_URI);
 
 const puppleSchema = new Schema({
+  imgUrl: String,
   name: String,
-  location: String,
+  location: { lat: String, lon: String },
   age: Number,
   personality: String,
   walkDuration: Number,
+  description: String,
 });
 
 module.exports = mongoose.model('Pupple', puppleSchema);
