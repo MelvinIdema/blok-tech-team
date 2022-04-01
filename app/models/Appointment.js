72f3e4941508c8ff223398228a5aa0869dd67c8e
Bram-ter <bram.tersteeg2@hva.nl>
2022-04-01 12:48:42 +0200

    #44 tweak appointment model

--- app/models/Appointment.js
+++ app/models/Appointment.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URI);
 
 const appointmentSchema = new mongoose.Schema({
+  imgUrl: String,
   date: Date,
   time: Number,
   name: String,
